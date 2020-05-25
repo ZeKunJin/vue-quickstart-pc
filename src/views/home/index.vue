d417e4b92634bbace89d7a9c232a61bc871e295c
jinzekun <zekun.jin@npi.org.cn>
2020-05-25 09:55:31 +0800

    remove components

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="home">
-    <info-page :config="config" />
+    home page
   </div>
-</template>
-
-<script>
-import InfoPage from '@/components/InfoPage'
-import config from './page'
-
-export default {
-  components: {
-    InfoPage
-  },
-
-  data() {
-    return {
-      config
-    }
-  }
-}
-</script>
+</template>、
